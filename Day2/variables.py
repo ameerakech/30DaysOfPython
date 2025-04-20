65b51d887c55381e8383f737a964560ccc18ef15
ameer akech <ameerakech@gmail.com>
2025-04-20 16:23:51 +0300

    first commit

---
+++ Day2/variables.py
@@ -0,0 +1,58 @@
+#'Day 2: 30 Days of python programming'
+first_name = 'Ameer'
+last_name ='Akech'
+full_name = 'Ameer Akech'
+country = 'South Sudan'
+city = 'Juba'
+age = 19
+year = 2025
+is_kenyan = False
+is_light_on = True
+
+#declaring multiples variables in one line 
+#first_name, last_name,full_name,age,country, city, age, year, is_kenyan, is_light_on = 'Ameer', 'Akech', 'South Sudan', 'Juba', 19, False, True
+
+#Exercise 2
+print(type('Ameer'))
+print(type('Akech'))
+print(type('Ameer Akech'))
+print(type('South Sudan'))
+print(type('Juba'))
+print(type(19))
+print(type(2025))
+print(type(False))
+print(type(True))
+print(len('Ameer'))
+
+
+num_one = 5
+num_two = 4
+total = num_one +num_two
+print(total)
+
+diff = num_one - num_two
+print(diff)
+
+product = num_one * num_two
+print(product)
+
+division = num_one / num_two
+print(division)
+
+remainder = num_one % num_two
+print(remainder)
+
+exp = num_one ** num_two
+print(exp)
+
+floor= num_one // num_two
+print(floor)
+
+area_of_circle = 3.14 * 30**2
+print(area_of_circle)
+
+circum_of_circle = 2 * 3.14 * 30**2
+print(circum_of_circle)
+
+help('keywords')
+
